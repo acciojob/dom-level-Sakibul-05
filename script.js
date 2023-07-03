9bf21cf0fc85ef4a14a1787f5004b5b0a0f5f2a6
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-03 12:22:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+// Wait for the DOM to load
+document.addEventListener('DOMContentLoaded', function() {
+  // Find the element with id "level"
+  var levelElement = document.getElementById('level');
+
+  // Count the number of parent elements
+  var level = 0;
+  var currentElement = levelElement;
+  while (currentElement.parentElement) {
+    level++;
+    currentElement = currentElement.parentElement;
+  }
+
+  // Display the level using an alert
+  alert('The level of the element is: ' + level);
+});
